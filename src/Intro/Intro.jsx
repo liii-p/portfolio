4817817a3dce6f8962b82f728aadd5c0c5a2edf9
lii-p <liannapyman@gmail.com>
2022-10-29 11:29:31 +1030

    Fixes intro paragraph formatting

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -12,12 +12,12 @@ const Intro = () => {
       <p className={styles.Intro__para}>
         Hi! I'm an avid nerd who reads books, plays tonnes of games and loves to
         code daily. <br />I love learning new things everyday so I can learn new
-        skills and improve myself. But when I'm not learning, I'm usually
+        skills and improve myself. <br /> But when I'm not learning, I'm usually
         playing games like the Batman Arkham series...or watching The Batman
         (2022) . <br /> Some might say I like Batman too much, but I believe you
         can never have too much Batman. <br />I love building React applications
         for fun, as well as challenging myself to learn new technologies and
-        languages. I even like to learn spoken languages like Korean and
+        languages. <br /> I even like to learn spoken languages like Korean and
         Chinese! <br />
         What am I currently working on? <br /> - This portfolio! <br /> - A
         react project inspired by Marvel's Daredevil (check it out{" "}
