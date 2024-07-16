8e4112bb664df121a271d8223b0e03031418414e
Lianna <liannapyman@gmail.com>
2024-07-16 21:43:43 +0930

    Updates outdated dependencies and edits Intro.jsx, adds headshot

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Intro.module.scss";
-import BatmanLogo from "../Images/Batman-Logo.png";
+import Headshot from "../Images/lianna-headshot2.JPG"
 
 const Intro = () => {
   return (
@@ -9,25 +9,30 @@ const Intro = () => {
         Lianna Pyman <br /> Software Developer
       </h1>
       <h2>About Me!</h2>
-      <p className={styles.Intro__para}>
-        Hi! I'm an avid nerd who reads books, plays tonnes of games and loves to
-        code daily. <br />I love learning new things everyday so I can learn new
-        skills and improve myself. <br /> But when I'm not learning, I'm usually
-        playing games like the Batman Arkham series...or watching The Batman
-        (2022) . <br />I love building React applications for fun, as well as
-        challenging myself to learn new technologies and languages such as
-        Node.js and AWS! <br /> I even like to learn spoken languages like
-        Korean and Chinese! <br />
-        <hr />
-        What am I currently working on? <br /> - This portfolio! <br /> - A
-        react project inspired by Marvel's Daredevil (check it out{" "}
-        <a href="https://github.com/liii-p/daredevil" target="blank">
-          here!
-        </a>
-        ) <br /> - An Angry Birds(TM) clone using unity game engine <br /> - And
-        more!
+      <div className={styles.Intro__flex}>
+        <div className={styles.Intro__para}>
+      <p>
+        Hi! I'm an eternal learner who reads books, watches movies and loves to
+        code daily. <br /> But when I'm not learning, I'm usually watching movies or
+        doing Muay Thai, hoping to become the next Karate Kid 
+        (even though that's a different martial art!). <br />I enjoy building React applications, as well as
+        challenging myself to learn new things such as Data Analytics or Azure! <br /> At the moment, I am pursuing my Bachelor of Computer Science with a double
+        major in Software Development and Cybersecurity. I'm always on the hunt to learn new things
+        and keep up to date! <br /> I always aim to improve my skills and get feedback so I can deliver
+        better value every day.</p>
+        </div>
+        <div className={styles.Intro__headshot}>
+          <img src={Headshot} alt="Lianna's headshot"/>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.Intro__para}>
+        <p>
+        What am I currently working on? <br /> - This portfolio! <br /> - University projects involving Python and Java.
+        Check out my Python project <a href="https://github.com/liii-p/codetown_burger_co">here</a>. 
+        <br /> - A trivia app using TypeScript and React, see it <a href="https://github.com/liii-p/trivia-app">here</a>.<br />
       </p>
-      <img src={BatmanLogo} className={styles.Intro__img} alt="batman logo" />
+      </div>
     </div>
   );
 };
